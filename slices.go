be97d04346ce47d646b3ec8145f877cc3ee11cdc
Forrest Ledbetter <forrest321@gmail.com>
2024-07-19 10:00:12 -0500

    used generics for slice ops

--- slices.go
+++ slices.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+type signedInts interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
 // small int slice to show basic handling
 // much of this is now included in std library
 var ints []int = []int{1, 2, 3, 4, 5}
@@ -30,21 +34,21 @@ func slices() {
 	bigSlice := makeBigSlice(15000)
 	fmt.Printf("bigSlice len is: %v\n", len(bigSlice))
 	fmt.Printf("bigSlice cap is: %v\n", cap(bigSlice))
-	fmt.Printf("new bigSlice sorted: %v\n", isSorted32(bigSlice))
+	fmt.Printf("new bigSlice sorted: %v\n", isSorted(bigSlice))
 	startTime := time.Now()
-	bigSlice = bubbleSort32(bigSlice)
+	bigSlice = bubbleSort(bigSlice)
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
 	fmt.Printf("bubble sort of bigSlice took: %v seconds\n", duration.Seconds())
-	fmt.Printf("bigSlice sorted: %v\n", isSorted32(bigSlice))
-	bigSlice = shuffle32(bigSlice)
-	fmt.Printf("shuffled bigSlice sorted: %v\n", isSorted32(bigSlice))
+	fmt.Printf("bigSlice sorted: %v\n", isSorted(bigSlice))
+	bigSlice = shuffle(bigSlice)
+	fmt.Printf("shuffled bigSlice sorted: %v\n", isSorted(bigSlice))
 	startTime2 := time.Now()
-	bigSlice = quickSort32(bigSlice)
+	bigSlice = quickSort(bigSlice)
 	endTime2 := time.Now()
 	duration2 := endTime2.Sub(startTime2)
 	fmt.Printf("quick sort of bigSlice took: %v seconds\n", duration2.Seconds())
-	fmt.Printf("bigSlice sorted: %v\n", isSorted32(bigSlice))
+	fmt.Printf("bigSlice sorted: %v\n", isSorted(bigSlice))
 	difference := duration - duration2
 	fmt.Printf("quick sort faster by %v seconds\n", difference.Seconds())
 }
@@ -57,9 +61,7 @@ func makeBigSlice(n int) []int32 {
 	return s
 }
 
-// The following methods can/should be genericized rather than
-// creating separate methods based on int type
-func shuffle(arr []int) []int {
+func shuffle[T signedInts](arr []T) []T {
 	for i := len(arr) - 1; i > 0; i-- {
 		if j := rand.Intn(i + 1); i != j {
 			arr[i], arr[j] = arr[j], arr[i]
@@ -68,25 +70,7 @@ func shuffle(arr []int) []int {
 	return arr
 }
 
-func shuffle32(arr []int32) []int32 {
-	for i := len(arr) - 1; i > 0; i-- {
-		if j := rand.Intn(i + 1); i != j {
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-	}
-	return arr
-}
-
-func isSorted(arr []int) bool {
-	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] > arr[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
-func isSorted32(arr []int32) bool {
+func isSorted[T signedInts](arr []T) bool {
 	for i := 0; i < len(arr)-1; i++ {
 		if arr[i] > arr[i+1] {
 			return false
@@ -97,7 +81,7 @@ func isSorted32(arr []int32) bool {
 
 // Sorting functions found here:
 // https://github.com/0xAX/go-algorithms/tree/master/sorting
-func bubbleSort(arr []int) []int {
+func bubbleSort[T signedInts](arr []T) []T {
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-1-i; j++ {
 			if arr[j] > arr[j+1] {
@@ -108,18 +92,7 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
-func bubbleSort32(arr []int32) []int32 {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-1-i; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
-		}
-	}
-	return arr
-}
-
-func quickSort(arr []int) []int {
+func quickSort[T signedInts](arr []T) []T {
 
 	if len(arr) <= 1 {
 		return arr
@@ -127,9 +100,9 @@ func quickSort(arr []int) []int {
 
 	median := arr[rand.Intn(len(arr))]
 
-	lowPart := make([]int, 0, len(arr))
-	highPart := make([]int, 0, len(arr))
-	middlePart := make([]int, 0, len(arr))
+	lowPart := make([]T, 0, len(arr))
+	highPart := make([]T, 0, len(arr))
+	middlePart := make([]T, 0, len(arr))
 
 	for _, item := range arr {
 		switch {
@@ -144,38 +117,6 @@ func quickSort(arr []int) []int {
 
 	lowPart = quickSort(lowPart)
 	highPart = quickSort(highPart)
-
-	lowPart = append(lowPart, middlePart...)
-	lowPart = append(lowPart, highPart...)
-
-	return lowPart
-}
-
-func quickSort32(arr []int32) []int32 {
-
-	if len(arr) <= 1 {
-		return arr
-	}
-
-	median := arr[rand.Intn(len(arr))]
-
-	lowPart := make([]int32, 0, len(arr))
-	highPart := make([]int32, 0, len(arr))
-	middlePart := make([]int32, 0, len(arr))
-
-	for _, item := range arr {
-		switch {
-		case item < median:
-			lowPart = append(lowPart, item)
-		case item == median:
-			middlePart = append(middlePart, item)
-		case item > median:
-			highPart = append(highPart, item)
-		}
-	}
-
-	lowPart = quickSort32(lowPart)
-	highPart = quickSort32(highPart)
 
 	lowPart = append(lowPart, middlePart...)
 	lowPart = append(lowPart, highPart...)
